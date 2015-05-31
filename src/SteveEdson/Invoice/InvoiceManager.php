4da94df28837ce3ab935ab9a3699e10fd3f3f606
Steve Edson <steve3d50n@gmail.com>
2015-05-31 16:35:50 +0100

    added method to get unpaid invoices

--- src/SteveEdson/Invoice/InvoiceManager.php
+++ src/SteveEdson/Invoice/InvoiceManager.php
@@ -5,4 +5,26 @@ namespace SteveEdson\Invoice;
 
 class InvoiceManager {
 
+    public $db;
+
+    function __construct(\PDO $db) {
+        $this->db = $db;
+    }
+
+    function getUnpaidInvoicesForAccount($account_id) {
+        $statement = $this->db->prepare("SELECT *
+                                         FROM invoice
+                                         WHERE account_id = :account_id
+                                          AND status = 'UNPAID'");
+
+        $result = $statement->execute([
+            "account_id" => $account_id
+        ]);
+
+        if($result) {
+            $invoices = $statement->fetchAll(\PDO::FETCH_CLASS, 'SteveEdson\Invoice\Invoice');
+        }
+
+        return $invoices;
+    }
 }
