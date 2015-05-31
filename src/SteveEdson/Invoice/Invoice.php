46c3ba3fa9a492cca01bea4b12e3e15f7b7f40cc
Steve Edson <steve3d50n@gmail.com>
2015-05-31 17:45:19 +0100

    updated fields in invoices

--- src/SteveEdson/Invoice/Invoice.php
+++ src/SteveEdson/Invoice/Invoice.php
@@ -6,11 +6,14 @@ namespace SteveEdson\Invoice;
 class Invoice {
     protected $id;
     protected $account_id;
-    protected $name;
+    protected $title;
+    protected $amount;
     protected $stripe_id;
     protected $description;
     protected $issue_date;
     protected $status;
+    protected $stripe_charge;
+    protected $payment_date;
 
     /**
      * @return mixed
@@ -59,5 +62,30 @@ class Invoice {
      */
     public function getStatus() {
         return $this->status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAmount() {
+        return $this->amount;
+    }
+
+    public function getStripeChargeId() {
+        return $this->stripe_charge;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPaymentDate() {
+        return $this->payment_date;
     }
 }
