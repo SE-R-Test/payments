c6cbabb6575577ad5be0ccd99cd0150920fa8bc9
Steve Edson <steve3d50n@gmail.com>
2015-05-31 17:47:34 +0100

    added routes to pay for invoices and view receipts

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@
 use SteveEdson\Account\AccountManager;
 use SteveEdson\Invoice\InvoiceManager;
 use Stripe\Invoice;
+use Stripe\Stripe;
 
 require __DIR__ . "/../vendor/autoload.php";
 
@@ -12,6 +13,8 @@ $app = new Slim\Slim(array(
     'view' => new \Slim\Views\Twig(),
     'templates.path' =>  __DIR__ . '/../template'
 ));
+
+$config = require __DIR__ . "/config.php";
 
 $app->container->singleton('db', function($container) {
     $config = require __DIR__ . "/config.php";
@@ -130,6 +133,39 @@ $app->get('/invoices/:id', function($id) use($app) {
     ]);
 
 })->name('invoices.view');
+
+$app->post('/invoices/:id/pay', function($id) use($app) {
+
+    $post = $app->request()->post();
+    $config = require __DIR__ . "/config.php";
+
+    $invoice_manager = new InvoiceManager($app->container->get('db'), $config['stripe']['test']['secret']);
+
+    $charge = $invoice_manager->payInvoice(
+        $app->container->get('account')->getId(),
+        $id,
+        $post['stripeToken'],
+        $post['stripeTokenType'],
+        $post['stripeEmail']
+    );
+
+    $app->redirect($app->urlFor('invoices.receipt', [ 'id' => $id ]));
+})->name('invoices.pay');
+
+$app->get('/invoices/:id/receipt', function($id) use ($app) {
+    $config = require __DIR__ . "/config.php";
+
+    $invoice_manager = new InvoiceManager($app->container->get('db'), $config['stripe']['test']['secret']);
+
+    $invoice = $invoice_manager->getUserInvoice($app->container->get('account')->getId(), $id);
+
+    $charge = $invoice_manager->getCharge($invoice->getStripeChargeId());
+
+    $app->render('invoice_receipt.twig', array(
+        'invoice' => $invoice,
+        'charge' => $charge
+    ));
+})->name('invoices.receipt');
 
 $app->get('/logout', function() use($app) {
 
